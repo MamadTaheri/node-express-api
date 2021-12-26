c81fff1b8cd534db6c561189fe16455cbb1dce3e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-26 21:42:37 +0330

    enhance post request

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
 const Post = require("./api/models/posts");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+
+const getExt = (mimeType) => {
+    switch (mimeType){
+        case "image/png":
+            return ".png";
+        case "image/jpeg":
+            return ".jpeg";
+    }
+}
+
+const upload = multer({storage: storage});
 const postsData = new Post();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -27,6 +49,20 @@ app.get("/api/posts/:post_id", (req, res) => {
     } else {
         res.status(404).send("not found");
     }
+})
+
+app.post("/api/posts",upload.single("post-image"), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    const newpost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": `uploads/${req.file.filename}`,
+        "added_date": `${Date.now()}`
+    }
+    postsData.add(newpost);
+    res.status(201).send("ok");
 })
 
 app.listen(3000, () => console.log("Listening on localhost:3000"))
