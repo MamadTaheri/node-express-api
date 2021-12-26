00dae6ac7fafd6b6330cd7724b7116cda11ec5af
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-26 17:31:56 +0330

    extend server

--- api/models/posts.js
+++ api/models/posts.js
@@ -8,18 +8,29 @@ class Post {
         return this.readData();
     }
 
-    getIndividualBlog(){
+    getIndividualBlog(postId){
         /** get One Blog post */
+        const posts = this.readData();
+        const foundPost = posts.find(q => q.id == postId);
+        return foundPost;
     }
 
-    add(){
+    add(newPost){
         /** Add new Post */
+        const currentposts= this.readData();
+        currentposts.unshift(newPost);
+        this.storeData(currentposts);
     }
 
     readData() {
         let rawData = fs.readFileSync(PATH);
         let posts = JSON.parse(rawData);
         return posts
+    }
+
+    storeData(rawData){
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
     }
 
 }
