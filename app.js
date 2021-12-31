b17ed76a2a0e3a22f37a928d20d9919ebf3445d3
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-31 15:11:45 +0330

    add appropriate messages to server

--- app.js
+++ app.js
@@ -131,29 +131,40 @@ const products = [{
 }]
 
 app.get('/current-user', (req, res) => {
+    console.log("request handling on progress....");
     res.json(currentUser);
+    console.log("successfully responded...");
 });
 
 app.get('/users', (req, res) => {
+    console.log("request handling on progress....");
     res.json(users);
+    console.log("successfully responded...");
 });
 
 app.get('/users/:id', (req, res) => {
     const { id } = req.params;
+    console.log(`requests user with id : ${id}  `);
 
     res.json(users.find(q => q.id === id));
+    console.log("successfully responded...");
 });
 
 app.get('/products', (req, res) => {
+    console.log("request handling on progress....");
     res.json(products);
+    console.log("successfully responded...");
 });
 
 app.get('/products/:id', (req, res) => {
     const { id } = req.params;
+    console.log(`requests product with id : ${id}  `);
 
     res.json(products.find(q => q.id == id));
+    console.log("successfully responded...");
 });
 
 app.listen(3333, () => {
+    console.log('Server Started');
     console.log('Server is listening on port 3333');
 });
