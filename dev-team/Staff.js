290c58a4e1aa7a93b439876a4b0409e3531484f1
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-01 21:56:43 +0430

    refactor entire project

---
+++ dev-team/Staff.js
@@ -0,0 +1,10 @@
+const mohammadTaheri = require("./01-mohammad-taheri");
+
+const emelMousavi = require("./02-Emel-Mousavi");
+
+const Staff = [
+    mohammadTaheri,
+    emelMousavi
+];
+
+module.exports = Staff;
