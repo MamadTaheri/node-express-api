5e0f64ef95dff9bf9e9a08826b4058e5fdfa8804
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-02 20:38:52 +0430

    update Staff

--- dev-team/Staff.js
+++ dev-team/Staff.js
@@ -2,9 +2,12 @@ const mohammadTaheri = require("./01-mohammad-taheri");
 
 const emelMousavi = require("./02-Emel-Mousavi");
 
+const saeedMohammadiha = require("./03-saeed-mohammadiha");
+
 const Staff = [
     mohammadTaheri,
-    emelMousavi
+    emelMousavi,
+    saeedMohammadiha,
 ];
 
 module.exports = Staff;
