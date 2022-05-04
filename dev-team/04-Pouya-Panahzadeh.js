f21845bb4af42a70d746d3cf30657774782119b4
Pouya <34831088+pou4a@users.noreply.github.com>
2022-05-04 21:57:24 +0430

    change like others

--- dev-team/04-Pouya-Panahzadeh.js
+++ dev-team/04-Pouya-Panahzadeh.js
@@ -5,7 +5,7 @@ const Pouyapanahzadeh = {
   linkedin: "https://www.linkedin.com/in/pouya-panahzadeh-678ba5b0/",
   github: "https://github.com/Pou4a",
   jobTitle: "Frontend Developer",
-  About: ` `,
+  About: `I am Pouya and want to change my career and work as a front-end developer `,
 };
 
 module.exports = Pouyapanahzadeh;
